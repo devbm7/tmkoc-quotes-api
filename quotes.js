fdc706c22e25cbfa91f98b00f77b49001aa921de
Dev Makwana <devmakwana7704@gmail.com>
2025-06-01 15:49:34 +0530

    v1.1.4

--- quotes.js
+++ quotes.js
@@ -23,5 +23,55 @@ module.exports = [
     quote: "Bhide hoon main, ek ek paise ka hisaab rakhta hoon!",
     author: "Bhide",
     tags: ["confident"]
+  },
+  {
+    quote: "Aayein? Baingan?",
+    author: "Jethalal",
+    tags: ["confused", "funny"]
+  },
+  {
+    quote: "Duniya Hila Denge Hum!",
+    author: "Sodhi",
+    tags: ["enthusiastic", "dramatic"]
+  },
+  {
+    quote: "Koi Kaam Chhota Nahi Hota Babuchak!",
+    author: "Dr.Hathi",
+    tags: ["wisdom", "motivational"]
+  },
+  {
+    quote: "Mere Paas Building Hai... Society Hai... Kya Hai?",
+    author: "Bagha",
+    tags: ["innocent", "funny"]
+  },
+  {
+    quote: "Arey O Samba... Kitna Deti Hai?",
+    author: "Abdul",
+    tags: ["sarcastic", "shopkeeper"]
+  },
+  {
+    quote: "Goli Beta... Masti Nahi!",
+    author: "Sodhi",
+    tags: ["strict", "caring"]
+  },
+  {
+    quote: "Jethalal... Yeh Sab Plan Tumhare Dimag Mein Kaise Aata Hai?",
+    author: "Taarak",
+    tags: ["amused", "questioning"]
+  },
+  {
+    quote: "Chup Kar Jetha!",
+    author: "Babitaji",
+    tags: ["angry", "annoyed"]
+  },
+  {
+    quote: "Main Apni Favourite Hoon!",
+    author: "Goli",
+    tags: ["funny", "self-praise"]
+  },
+  {
+    quote: "Daya... Bring Me My Tea!",
+    author: "Jethalal",
+    tags: ["demanding", "comic"]
   }
 ];
